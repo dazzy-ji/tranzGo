fa8d912b2f34601144a006d60ae7ebeaa360bde3
dazzy-ji <d.indimuli@alustudent.com>
2026-07-23 20:28:30 +0200

    Merge issue fixed

--- tranzgo.py
+++ tranzgo.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+
+# Section 1: Imports
+import difflib
+
+from db_config import connection
+
+# How each means of transport changes the bus fare and time.
+# For example a motorbike costs 2.5x the bus fare but takes 0.5x the time.
+MODES = {
+    "bus":       {"fare": 1.0, "time": 1.0},
+    "motorbike": {"fare": 2.5, "time": 0.5},
+    "car":       {"fare": 4.0, "time": 0.7},
+}
+
+# Section 2: Trip Class
 class Trip:
     """A single trip between two places."""
 
@@ -14,17 +30,3 @@ class Trip:
     def time(self, mode):
         """Travel time for this trip using the chosen means of transport."""
         return round(self.travel_time_min * MODES[mode]["time"])
-#!/usr/bin/env python3
-
-# Section 1: Imports
-import difflib
-
-from db_config import connection
-
-# How each means of transport changes the bus fare and time.
-# For example a motorbike costs 2.5x the bus fare but takes 0.5x the time.
-MODES = {
-    "bus":       {"fare": 1.0, "time": 1.0},
-    "motorbike": {"fare": 2.5, "time": 0.5},
-    "car":       {"fare": 4.0, "time": 0.7},
-}
