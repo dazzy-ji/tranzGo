d4b6d009906d868b0db081c5fb664e2390ce8faa
dazzy-ji <d.indimuli@alustudent.com>
2026-07-25 19:54:28 +0200

    connection file

--- db_config.py
+++ db_config.py
@@ -6,12 +6,12 @@ import mysql.connector
 
 try:
     connection = mysql.connector.connect(
-        host='',
-        port=,
-        user='',
-        password='',
-        database='',
-        ssl_disabled=
+        host='mysql-dj-alustudent-dj.i.aivencloud.com',
+        port=16865,
+        user='avnadmin',
+        password='AVNS_HmRG-kI36oO_dv4ELa-',
+        database='defaultdb',
+        ssl_disabled=False
     )
 except mysql.connector.Error as e:
     print("Error: could not connect to the TranzGo database.")
